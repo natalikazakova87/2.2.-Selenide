5ff8c4f31276c620fbf4edc2247ee88e3e8b71c9
natalikazakova <natalikazakova87@gmail.com>
2023-08-27 22:36:37 +0700

    initial commit

--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -17,15 +17,15 @@ public class DeliveryCardTest {
     public void shouldSuccessForm() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Новосибирск");
-        String currentDate = generateDate(6, "dd.mm.yyyy");
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME));
-        $("[data-test-id='date'] input").sendKeys(currentDate);
+        String planningDate = generateDate(4, "dd.mm.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Казакова Наталья");
         $("[data-test-id='phone'] input").setValue("+79635489658");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content").shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
     }
 
 }
